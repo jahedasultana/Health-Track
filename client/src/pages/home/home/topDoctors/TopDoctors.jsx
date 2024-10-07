414bca04cbf4dec55de48f88b4ed0cad7d40e528
jaheda8256 <jahedasultana47@gmail.com>
2024-10-07 23:20:55 +0600

    create new section

--- client/src/pages/home/home/topDoctors/TopDoctors.jsx
+++ client/src/pages/home/home/topDoctors/TopDoctors.jsx
@@ -2,17 +2,17 @@
 const TopDoctors = () => {
     return (
         <div className="grid md:grid-cols-3 grid-cols-1 w-[85%] mx-auto">
-            <div className="p-8 bg-[#58d6da] text-white space-y-4">
+            <div className="p-10 bg-[#4fcfd3] text-white space-y-4">
                 <h2 className="text-[25px]">Top Doctors</h2>
             <p>Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augue tristique sed volutpat.</p>
             <button className="px-3 py-2 border border-gray-100/50">Read More</button>
             </div>
-            <div className="p-8 bg-[#05bdca] text-white space-y-4">
+            <div className="p-10 bg-[#05bdca] text-white space-y-4">
                 <h2 className="text-[25px]">24 Hours Service</h2>
             <p>Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augue tristique sed volutpat.</p>
             <button className="px-3 py-2 border border-gray-100/50">Read More</button>
             </div>
-            <div className="p-8 bg-[#02a0ac] text-white space-y-4">
+            <div className="p-10 bg-[#02a0ac] text-white space-y-4">
                 <h2 className="text-[25px]">Opening Hours</h2>
             <p>Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augue tristique sed volutpat.</p>
             <button className="px-3 py-2 border border-gray-100/50">Read More</button>
