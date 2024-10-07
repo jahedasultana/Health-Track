29be37fdb84016dc0c86fa2c7c6db28aefb0dc1c
jaheda8256 <jahedasultana47@gmail.com>
2024-10-07 22:54:51 +0600

    create TopDoctore

--- client/src/pages/home/home/topDoctors/TopDoctors.jsx
+++ client/src/pages/home/home/topDoctors/TopDoctors.jsx
@@ -2,18 +2,18 @@
 const TopDoctors = () => {
     return (
         <div className="grid md:grid-cols-3 grid-cols-1 w-[85%] mx-auto">
-            <div className=" p-4 bg-[#58d6da] text-white">
-                <h2 className="text-[20px]">Top Doctors</h2>
+            <div className=" p-4 bg-[#58d6da] text-white space-y-4">
+                <h2 className="text-[25px]">Top Doctors</h2>
             <p>Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augue tristique sed volutpat.</p>
             <button className="p-4">Read More</button>
             </div>
-            <div className="p-4 bg-[#05bdca] text-white">
-                <h2 className="text-[20px]">Top Doctors</h2>
+            <div className="p-4 bg-[#05bdca] text-white space-y-4">
+                <h2 className="text-[25px]">24 Hours Service</h2>
             <p>Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augue tristique sed volutpat.</p>
             <button className="p-4">Read More</button>
             </div>
-            <div className="p-4 bg-[#048791] text-white">
-                <h2 className="text-[20px]">Top Doctors</h2>
+            <div className="p-4 bg-[#02a0ac] text-white space-y-4">
+                <h2 className="text-[25px]">Opening Hours</h2>
             <p>Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augue tristique sed volutpat.</p>
             <button className="p-4">Read More</button>
             </div>
