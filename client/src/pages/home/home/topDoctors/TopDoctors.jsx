db4fae36fed51784016c569fe0b263074c983b4f
jaheda8256 <jahedasultana47@gmail.com>
2024-10-08 09:42:45 +0600

    added date

--- client/src/pages/home/home/topDoctors/TopDoctors.jsx
+++ client/src/pages/home/home/topDoctors/TopDoctors.jsx
@@ -14,8 +14,22 @@ const TopDoctors = () => {
             </div>
             <div className="p-10 bg-[#02a0ac] text-white space-y-4">
                 <h2 className="text-[25px]">Opening Hours</h2>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augue tristique sed volutpat.</p>
-            <button className="px-3 py-2 border border-gray-100/50">Read More</button>
+           <div className="flex justify-between">
+           <p>Monday-friday</p>
+           <p>8.00-17.00</p>
+           </div>
+           <hr className="opacity-60"/>
+           <div className="flex justify-between">
+           <p>Saturday</p>
+           <p>9.30-17.30</p>
+           </div>
+           <hr className="opacity-60"/>
+           <div className="flex justify-between">
+           <p>Sunday</p>
+           <p>9.00-15.00</p>
+           </div>
+           
+        
             </div>
            
         </div>
