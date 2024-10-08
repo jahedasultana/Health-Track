56667a884aec875ca256f9cb7d5c7125150b09b8
jaheda8256 <jahedasultana47@gmail.com>
2024-10-08 20:30:05 +0600

    added bg-color

--- client/src/pages/home/ourServices/OurServices.jsx
+++ client/src/pages/home/ourServices/OurServices.jsx
@@ -2,18 +2,20 @@
 
 const OurServices = () => {
     return (
-        <div className="flex gap-4 w-[85%] mx-auto">
-            <div>
-
+        <div className="bg-[#E4FEFF]">
+            <div className="flex my-16 gap-4 w-[85%] mx-auto">
+            <div className="w-[20%]">
+            <img src="https://i.postimg.cc/0yFgrV49/hayd.jpg" alt="service" />
             </div>
 
-            <div>
+            <div className="w-[40%]">
                 <img src="https://i.postimg.cc/mrxGrdc7/happy-doctor.jpg" alt="doctorService" />
             </div>
 
-            <div>
+            <div className="w-[40%]">
                 <img src="https://i.postimg.cc/4xRgfLHV/hospital-reception.jpg" alt="seatBooking" />
             </div>
+        </div>
         </div>
     );
 };
