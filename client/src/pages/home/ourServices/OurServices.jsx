d3c70d06df61fa9635f95cbca5f923ce0332eed7
jaheda8256 <jahedasultana47@gmail.com>
2024-10-08 22:01:44 +0600

    added bg-image

--- client/src/pages/home/ourServices/OurServices.jsx
+++ client/src/pages/home/ourServices/OurServices.jsx
@@ -1,23 +1,45 @@
-
-
 const OurServices = () => {
-    return (
-        <div className="bg-[#E4FEFF]">
-            <div className="flex my-16 gap-4 w-[85%] mx-auto">
-            <div className="w-[20%]">
-            <img src="https://i.postimg.cc/0yFgrV49/hayd.jpg" alt="service" />
-            </div>
+  return (
+    <div className="bg-[#E4FEFF] relative">
+        <h1 className="absolute text-black font-bold right-0  -top-10 opacity-5 text-[100px]">Our Services</h1>
+      <div className="flex my-16 gap-4 w-[85%] mx-auto">
+        {/* First image container */}
 
-            <div className="w-[40%]">
-                <img src="https://i.postimg.cc/mrxGrdc7/happy-doctor.jpg" alt="doctorService" />
-            </div>
+        <div className="w-[20%] relative mr-3">
+          <img
+            className="h-[350px] w-full object-cover"
+            src="https://i.postimg.cc/0yFgrV49/hayd.jpg"
+            alt="service"
+          />
 
-            <div className="w-[40%]">
-                <img src="https://i.postimg.cc/4xRgfLHV/hospital-reception.jpg" alt="seatBooking" />
-            </div>
+          {/* Overlay */}
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <h3 className="text-[30px] text-white font-semibold -rotate-90">
+              Our Services
+            </h3>
+          </div>
         </div>
+
+        {/* Second image container with bottom alignment */}
+        <div className="w-[40%] flex items-end">
+          <img
+            className="w-full object-cover"
+            src="https://i.postimg.cc/mrxGrdc7/happy-doctor.jpg"
+            alt="doctorService"
+          />
         </div>
-    );
+
+        {/* Third image container */}
+        <div className="w-[40%] flex items-end">
+          <img
+            className="w-full object-cover"
+            src="https://i.postimg.cc/4xRgfLHV/hospital-reception.jpg"
+            alt="seatBooking"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurServices;
