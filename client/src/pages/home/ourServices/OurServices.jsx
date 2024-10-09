99e4f03457e6d22f25310b686ce1fcd7f22bef53
jaheda8256 <jahedasultana47@gmail.com>
2024-10-09 20:39:58 +0600

    added hover class

--- client/src/pages/home/ourServices/OurServices.jsx
+++ client/src/pages/home/ourServices/OurServices.jsx
@@ -33,14 +33,16 @@ const OurServices = () => {
 
             {/* Title and Button (Always Visible) */}
             <div className="absolute inset-0 flex flex-col justify-center bg-black bg-opacity-20  items-center text-white z-10">
-              <h2 className="text-2xl font-semibold">Doctor Appointment</h2>
-              <button className="mt-4 px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600">
+              <h2 className="text-3xl text-[#eef0f4] font-semibold">Doctor Appointment</h2>
+              <h3 className="text-xl text-[#eef0f4] font-semibold">Total Doctor: 101</h3>
+              <h5 className="text-lg text-[#eef0f4] font-semibold">category: 10</h5>
+              <button className="mt-4 px-4 py-2 bg-[#1DBFCC] rounded-md hover:bg-[#08d7e6]">
                 Learn More
               </button>
             </div>
 
             {/* Overlay (Initially Hidden, Slides in from Left on Hover) */}
-            <div className="absolute inset-0 bg-gradient-to-l from-black to-transparent translate-x-[-100%] opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-500 ease-in-out"></div>
+            <div className="absolute inset-0 bg-gradient-to-l from-[#1DBFCC] to-transparent translate-x-[-100%] opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-500 ease-in-out"></div>
           </div>
         </div>
         <div className="w-[40%] flex items-end rounded-md">
@@ -53,14 +55,17 @@ const OurServices = () => {
 
             {/* Title and Button (Always Visible) */}
             <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-20 text-white z-10">
-              <h2 className="text-2xl font-semibold">Seat booking</h2>
-              <button className="mt-4 px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600">
+              <h2 className="text-3xl text-[#eef0f4] font-semibold">Seat Booking</h2>
+              <h3 className="text-xl text-[#eef0f4] font-semibold">Total seat: 101</h3>
+              <h5 className="text-lg text-[#eef0f4] font-semibold">category: 5</h5>
+             
+              <button className="mt-4 px-4 py-2 bg-[#1DBFCC] rounded-md hover:bg-[#08d7e6]">
                 Learn More
               </button>
             </div>
 
             {/* Overlay (Initially Hidden, Slides in from Left on Hover) */}
-            <div className="absolute inset-0 bg-gradient-to-l from-black to-transparent translate-x-[-100%] opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-500 ease-in-out"></div>
+            <div className="absolute inset-0 bg-gradient-to-l from-[#1DBFCC] to-transparent translate-x-[-100%] opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-500 ease-in-out"></div>
           </div>
         </div>
       
