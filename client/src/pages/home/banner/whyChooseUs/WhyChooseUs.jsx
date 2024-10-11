2774f9548c71389f96345a8cc936c90a2f384ea5
jaheda8256 <jahedasultana47@gmail.com>
2024-10-11 20:44:28 +0600

    added why choose us section image

--- client/src/pages/home/banner/whyChooseUs/WhyChooseUs.jsx
+++ client/src/pages/home/banner/whyChooseUs/WhyChooseUs.jsx
@@ -2,12 +2,12 @@
 
 const WhyChooseUs = () => {
     return (
-        <div className="flex gap-5 items-center w-[90%] mx-auto">
-          <div className="md:w-[50%] w-full">
-            <img className="h-[400px]" src="https://i.postimg.cc/ry0NVkbP/hand-holding.jpg" alt="whyChoose" />
+        <div className="flex my-12 gap-5 items-center w-[90%] mx-auto">
+          <div className="md:w-[62%] w-full">
+            <img className="h-[400px] w-full object-cover" src="https://i.postimg.cc/ry0NVkbP/hand-holding.jpg" alt="whyChoose" />
           </div>
-          <div className="md:w-[50%] w-full">
-            <p>Streamlined Appointment Scheduling</p>
+          <div className="md:w-[38%] w-full text-[18px] font-semibold space-y-3">
+            <p>Streamlined Appointment Scheduling.</p>
             <p>Real-time Analytics & Reporting</p>
             <p>Remote Access & Telemedicine Integration</p>
             <p>Data Security and Privacy</p>
