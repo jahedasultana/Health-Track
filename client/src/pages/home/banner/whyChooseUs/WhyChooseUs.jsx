eeb693fab4de62758a9b18429ce6cd3c097109d4
jaheda8256 <jahedasultana47@gmail.com>
2024-10-11 20:59:31 +0600

    added bg-color

--- client/src/pages/home/banner/whyChooseUs/WhyChooseUs.jsx
+++ client/src/pages/home/banner/whyChooseUs/WhyChooseUs.jsx
@@ -2,8 +2,11 @@
 
 const WhyChooseUs = () => {
     return (
-        <div className="flex my-12 gap-5 items-center w-[90%] mx-auto">
-          <div className="md:w-[62%] w-full">
+        <div className="flex relative my-12 gap-5 items-center w-[90%] mx-auto">
+            <div className="absolute -top-6 -left-[60px] bg-black/20 h-64 z-0 w-44">
+            <p className="-rotate-90">Why Choose Us</p>
+            </div>
+          <div className="md:w-[62%] z-10 w-full">
             <img className="h-[400px] w-full object-cover" src="https://i.postimg.cc/ry0NVkbP/hand-holding.jpg" alt="whyChoose" />
           </div>
           <div className="md:w-[38%] w-full text-[18px] font-semibold space-y-3">
