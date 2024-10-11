8ec159140421ba75f59388d016883878a82967b9
jaheda8256 <jahedasultana47@gmail.com>
2024-10-11 23:35:17 +0600

    added icon

--- client/src/pages/home/banner/whyChooseUs/WhyChooseUs.jsx
+++ client/src/pages/home/banner/whyChooseUs/WhyChooseUs.jsx
@@ -2,7 +2,7 @@ import { MdHealthAndSafety, MdSchedule, MdInsights, MdSecurity, MdSupportAgent }
 
 const WhyChooseUs = () => {
     return (
-        <div className="flex relative my-12 gap-10 items-center w-[90%] mx-auto">
+        <div className="flex relative my-12 gap-12 items-center w-[90%] mx-auto">
 
             {/* Background Box */}
             <div className="absolute  -top-6 -left-[63px] bg-[#1DBFCC] h-[320px] z-0 w-[320px]">
@@ -14,14 +14,15 @@ const WhyChooseUs = () => {
             {/* Image with overlay */}
             <div className="relative md:w-[50%] z-10 w-full">
                 <img className="h-[400px] w-full object-cover" src="https://i.postimg.cc/ry0NVkbP/hand-holding.jpg" alt="whyChoose" />
-                <div className="absolute inset-0 bg-black/40 z-10"></div> {/* Overlay */}
+                <div className="absolute inset-0 bg-black/20 z-10"></div> {/* Overlay */}
             </div>
 
             {/* Text and Icons */}
             <div className="md:w-[50%] w-full text-[18px] font-semibold space-y-3 text-right"> {/* Align text right */}
+
                 {/* content 1 */}
-                <div className="flex w-full items-center gap-5 justify-end"> {/* Align icon and text to the right */}
-                    <div className="bg-[#991747] rounded-full">
+                <div className="flex w-full items-center gap-5 justify-start"> {/* Align icon and text to the right */}
+                    <div className="bg-[#1DBFCC] rounded-full">
                         <p className="md:h-16 h-12 md:w-16 w-12 text-xl font-semibold flex items-center justify-center text-white">
                             <MdHealthAndSafety size={25} />
                         </p>
@@ -32,8 +33,8 @@ const WhyChooseUs = () => {
                 </div>
 
                 {/* content 2 */}
-                <div className="flex items-center gap-5 justify-end">
-                    <div className="bg-[#991747] rounded-full">
+                <div className="flex items-center gap-5 justify-start">
+                    <div className="bg-[#1DBFCC] rounded-full">
                         <p className="md:h-16 h-12 md:w-16 w-12 text-xl font-semibold flex items-center justify-center text-white">
                             <MdInsights size={25} />
                         </p>
@@ -44,8 +45,8 @@ const WhyChooseUs = () => {
                 </div>
 
                 {/* content 3 */}
-                <div className="flex items-center gap-5 justify-end">
-                    <div className="bg-[#991747] rounded-full">
+                <div className="flex items-center gap-5 justify-start">
+                    <div className="bg-[#1DBFCC] rounded-full">
                         <p className="md:h-16 h-12 md:w-16 w-12 text-xl font-semibold flex items-center justify-center text-white">
                             <MdSchedule size={25} />
                         </p>
@@ -56,8 +57,8 @@ const WhyChooseUs = () => {
                 </div>
 
                 {/* content 4 */}
-                <div className="flex items-center gap-5 justify-end">
-                    <div className="bg-[#991747] rounded-full">
+                <div className="flex items-center gap-5 justify-start">
+                    <div className="bg-[#1DBFCC] rounded-full">
                         <p className="md:h-16 h-12 md:w-16 w-12 text-xl font-semibold flex items-center justify-center text-white">
                             <MdSecurity size={25} />
                         </p>
@@ -68,8 +69,8 @@ const WhyChooseUs = () => {
                 </div>
 
                 {/* content 5 */}
-                <div className="flex items-center gap-5 justify-end">
-                    <div className="bg-[#991747] rounded-full">
+                <div className="flex items-center gap-5 justify-start">
+                    <div className="bg-[#1DBFCC] rounded-full">
                         <p className="md:h-16 h-12 md:w-16 w-12 text-xl font-semibold flex items-center justify-center text-white">
                             <MdSupportAgent size={25} />
                         </p>
