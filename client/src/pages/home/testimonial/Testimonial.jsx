b77945f6c132d74ad421771ff2370e4cfd8fddca
jaheda8256 <jahedasultana47@gmail.com>
2024-10-24 22:28:14 +0600

    added bg-colors

--- client/src/pages/home/testimonial/Testimonial.jsx
+++ client/src/pages/home/testimonial/Testimonial.jsx
@@ -3,13 +3,13 @@
 const Testimonial = () => {
     return (
         <div className="flex w-[90%] mx-auto">
-            <div className="w-[35%] space-y-4">
-                <h6 className="text-xl font-normal text-[#1DBFCC]">20+ Years Of Experiences</h6>
+            <div className="w-[35%] bg-[#FFFFFF] space-y-4">
+                <h6 className="text-lg font-semibold text-[#1DBFCC]">20+ Years Of Experiences</h6>
                 <h1 className="md:text-4xl text-xl font-bold text-[#3f3f41]">What Our Patients Say About Us</h1>
                 <p className="text-gray-600">Adipiscing elitadi piscing elits eddo eusmod teo mpor incididunt utlabore edolor magnased doe iusmod tempor incididunt utlaboreet dolore ma gna aliqua. Quis ipsum suspend isseultrices and gravida. Consectetur adipiscing elitadipiscing in elitserissed aliqua.</p>
             </div>
 
-            <div className="w-[65%]">
+            <div className="w-[65%] bg-[#F4F7F9]">
                 <h1>hi</h1>
             </div>
         </div>
