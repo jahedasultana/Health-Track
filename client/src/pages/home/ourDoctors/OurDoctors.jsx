ffcb3094175924319ade6ee72a62a292ce58eb63
jaheda8256 <jahedasultana47@gmail.com>
2024-10-26 20:30:10 +0600

    added Our Doctors

--- client/src/pages/home/ourDoctors/OurDoctors.jsx
+++ client/src/pages/home/ourDoctors/OurDoctors.jsx
@@ -1,5 +1,5 @@
-import { FaPhoneAlt, FaEnvelope, FaPlus } from "react-icons/fa";
-
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa6";
 const OurDoctors = () => {
   return (
     <section className="w-[90%] mx-auto">
@@ -7,18 +7,15 @@ const OurDoctors = () => {
             <p className="text-[#1DBFCC]">Professionals</p>
             <h1 className="md:text-3xl text-xl font-medium text-[#3f3f41]">Our Doctors</h1>
         </div>
-      <div className="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg relative group">
-        <div className="relative">
-          <img
-            className="object-cover w-full h-56 transition-transform duration-300 group-hover:scale-y-110"
-            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            alt="avatar"
-          />
-          <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300 flex items-center justify-center">
-            <FaPlus className="text-white text-3xl" />
-          </div>
-        </div>
+      <div className="w-full max-w-xs bg-white rounded-lg shadow-lg ">
+      
+        <div className="overflow-hidden relative group">
+            <img className="group-hover:scale-105 transition-all duration-700" src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
 
+            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300 flex items-center justify-center">
+                        <FaPlus className="text-white text-3xl" />
+                    </div>
+        </div>
         <div className="py-5 text-center">
           <h3 className="text-3xl text-[#1DBFCC] ">Dr. John Doe</h3>
           <p className="text-lg opacity-70 text-gray-700">Software Engineer</p>
