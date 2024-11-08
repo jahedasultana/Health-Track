c59e895c8d668d046c109d61ddb377c78639fd5f
jaheda8256 <jahedasultana47@gmail.com>
2024-11-08 21:09:53 +0600

    aboutUs page fix and added h tag

--- client/src/pages/home/aboutUs/AboutUs.jsx
+++ client/src/pages/home/aboutUs/AboutUs.jsx
@@ -23,6 +23,7 @@ const AboutUs = () => {
 
         {/* Text Section */}
         <div className="w-full md:w-1/2 text-left">
+        <h2 className="text-xl font-medium text-[#1ABC9C] pb-3">About Us_</h2>
           <h2 className="text-3xl font-bold text-gray-800">
             Consult A Doctor Anytime, Anywhere By Search
           </h2>
