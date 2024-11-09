11b814518f0c82f95cf3dce7d586d804abb64b90
jaheda8256 <jahedasultana47@gmail.com>
2024-11-09 17:30:51 +0600

    faq expand fix

--- client/src/pages/home/faq/Faq.jsx
+++ client/src/pages/home/faq/Faq.jsx
@@ -66,35 +66,39 @@ const Faq = () => {
             <FaQuestion className="text-[#1DBFCC]" />
             Frequently Asked Questions
           </h2> */}
-          {faqs.map((faq, index) => (
-            <div key={index} className="border-b  p-4">
-              <div
-                className="flex justify-between items-center cursor-pointer"
-                onClick={() => toggleFAQ(index)}
-              >
-                <h3 className="text-base md:w-full w-[90%] text-[#6e6e6f] font-medium flex items-center gap-4"><FaQuestionCircle className={`text-xl ${
-                    open == null ? "text-red-700" : "text-[#1DBFCC]"
-                  }`}/>
-                  {faq.question}
-                </h3>
-                <button
-                  className={`text-xl font-semibold w-7 h-7 text-white ${
-                    open == null ? "bg-red-700" : "bg-[#1DBFCC]"
-                  } rounded-full`}
-                  aria-expanded={open === index}
-                >
-                  {open === index ? "-" : "+"}
-                </button>
-              </div>
-              <div
-                className={`mt-6 text-gray-600 transition-all duration-300 ease-in-out ${
-                  open === index ? "block" : "hidden"
-                }`}
-              >
-                {faq.answer}
-              </div>
-            </div>
-          ))}
+        {faqs.map((faq, index) => (
+  <div key={index} className="border-b p-4">
+    <div
+      className="flex justify-between items-center cursor-pointer"
+      onClick={() => toggleFAQ(index)}
+    >
+      <h3 className="text-base md:w-full w-[90%] text-[#6e6e6f] font-medium flex items-center gap-4">
+        <FaQuestionCircle
+          className={`text-xl ${
+            open === index ? "text-[#1DBFCC]" : "text-red-700"
+          }`}
+        />
+        {faq.question}
+      </h3>
+      <button
+        className={`text-xl font-semibold w-7 h-7 text-white ${
+          open === index ? "bg-[#1DBFCC]" : "bg-red-700"
+        } rounded-full`}
+        aria-expanded={open === index}
+      >
+        {open === index ? "-" : "+"}
+      </button>
+    </div>
+    <div
+      className={`mt-6 text-gray-600 transition-all duration-500 ease-in-out overflow-hidden ${
+        open === index ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+      }`}
+    >
+      {faq.answer}
+    </div>
+  </div>
+))}
+
         </div>
       </div>
     </div>
