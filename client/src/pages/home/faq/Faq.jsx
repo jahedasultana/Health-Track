354db86d77ac7487d8b13341f0b6d9d22a193760
jaheda8256 <jahedasultana47@gmail.com>
2024-11-10 16:59:55 +0600

    fix faq section

--- client/src/pages/home/faq/Faq.jsx
+++ client/src/pages/home/faq/Faq.jsx
@@ -3,8 +3,12 @@ import TopIcon from "./TopIcon";
 import LeftIcon from "./LeftIcon";
 import HeartIcon from "./HeartIcon";
 import { FaQuestionCircle } from "react-icons/fa";
+
+import { RiArrowDropUpLine } from "react-icons/ri";
+import { RiArrowDropDownLine } from "react-icons/ri";
+
 const Faq = () => {
-    const [open, setOpen] = useState(null);
+  const [open, setOpen] = useState(null);
 
   const toggleFAQ = (index) => {
     setOpen(open === index ? null : index);
@@ -37,57 +41,59 @@ const Faq = () => {
     },
   ];
 
-    return (
-        <div className=" w-[90%] mx-auto ">
-            <div className=" flex flex-col md:flex-row gap-8">
-                {/* FAQ Image with Overlay Text */}
-                <div className="md:w-[50%] w-full mt-6 md:mt-0 relative">
-                    <img
-                        src="https://kamleshyadav.com/html/healthcare/bootstrap5/demo9/assets/images/faq.png"
-                        alt="faq"
-                        className="w-full h-auto"
-                    />
+  return (
+    <div className=" w-[90%] mx-auto ">
+      <div className=" flex flex-col md:flex-row gap-10">
+        {/* FAQ Image with Overlay Text */}
+        <div className="md:w-[50%] w-full mt-6 md:mt-0 relative">
+          <img
+            src="https://kamleshyadav.com/html/healthcare/bootstrap5/demo9/assets/images/faq.png"
+            alt="faq"
+            className="w-full h-auto"
+          />
 
-                    <div className="absolute -top-10 right-14">
-                        <TopIcon></TopIcon>
-                    </div>
-                    <div className="absolute bottom-12 -left-5">
-                        <LeftIcon></LeftIcon>
-                    </div>
-                    <div className="absolute bottom-14 right-[1px]">
-                        <HeartIcon></HeartIcon>
-                    </div>
-                </div>
-
-                {/* FAQ Section */}
-               <div className="md:w-[50%] w-full mx-auto h-[175px]">
-        {faqs.map((faq, index) => (
-        <div key={index} className="mb-4 border-b pb-2">
-          <div
-            className="flex justify-between items-center cursor-pointer p-3"
-            onClick={() => toggleFAQ(index)}
-          >
-            <h3 className="text-lg font-medium">{faq.question}</h3>
-            <button
-              className="text-xl font-normal w-7 h-7 text-white bg-[#991747] rounded-full"
-              aria-expanded={open === index}
-            >
-              {open === index ? "-" : "+"}
-            </button>
+          <div className="absolute -top-10 right-14">
+            <TopIcon></TopIcon>
           </div>
-          <div
-            className={`mt-2 p-3 text-gray-600 transition-all duration-300 ease-in-out ${
-              open === index ? "block" : "hidden"
-            }`}
-          >
-            {faq.answer}
+          <div className="absolute bottom-12 -left-5">
+            <LeftIcon></LeftIcon>
+          </div>
+          <div className="absolute bottom-14 right-[1px]">
+            <HeartIcon></HeartIcon>
           </div>
         </div>
-        ))}
-      </div>
+
+        {/* FAQ Section */}
+        <div className="md:w-[50%] w-full mx-auto h-[175px]">
+          {faqs.map((faq, index) => (
+            <div key={index} className="mb-4 bg-gray-200 border pb-2">
+              <div
+                className="flex justify-between items-center cursor-pointer p-[14px]"
+                onClick={() => toggleFAQ(index)}
+              >
+                <h3 className="text-base font-medium  flex items-center gap-4 transition-all duration-700 delay-500"><FaQuestionCircle className={`text-lg ${
+                    open === index ? "text-[#1DBFCC]" : "text-red-700"
+                  }`}></FaQuestionCircle>{faq.question}</h3>
+                <button
+                  
+                  aria-expanded={open === index}
+                >
+                  {open === index ? <RiArrowDropUpLine className="text-4xl"/> : <RiArrowDropDownLine className="text-4xl"/>}
+                </button>
+              </div>
+              <div
+                className={`mt-2 p-3 text-gray-600 transition-all duration-300 ease-in-out ${
+                  open === index ? "block" : "hidden"
+                }`}
+              >
+                {faq.answer}
+              </div>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
