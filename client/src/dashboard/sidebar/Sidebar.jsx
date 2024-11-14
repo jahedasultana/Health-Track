6ba6114a30fddffb24213224a4fb3343b3e6f90b
jaheda8256 <jahedasultana47@gmail.com>
2024-11-14 19:54:12 +0600

    icon set the dashboard

--- client/src/dashboard/sidebar/Sidebar.jsx
+++ client/src/dashboard/sidebar/Sidebar.jsx
@@ -1,18 +1,40 @@
 import "./Sidebar.css";
 import Logo from "../../assets/dashboardImage/logo (1).png";
-
+import { SidebarData } from "../data/Data";
+import {UilSignOutAlt} from "@iconscout/react-unicons"
 const Sidebar = () => {
-    return (
-        <div className="Sidebar">
-            {/* logo */}
-            <div className="logo">
-                <img src={Logo} alt="" />
-                <span>
-                    Sh<span>o</span>ps
-                </span>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Sidebar">
+      {/* logo */}
+      <div className="logo">
+        <img src={Logo} alt="" />
+        <span>
+          Sh<span>o</span>ps
+        </span>
+      </div>
+
+      {/* menu */}
+      <div className="menu">
+       {
+        SidebarData.map((item, index) =>{
+            return(
+                <div key={item.heading} className="menuItem">
+                    <item.icon/>
+                    <span>
+                        {item.heading}
+                    </span>
+                </div>
+            )
+        })
+       }
+
+       <div className="menuItem">
+            <UilSignOutAlt/>
+       </div>
+       
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
