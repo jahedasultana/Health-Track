3b3e8f338476c9b270859f2c9e159bccb2330884
jaheda8256 <jahedasultana47@gmail.com>
2024-11-14 18:27:40 +0600

    change text colors

--- client/src/dashboard/sidebar/Sidebar.jsx
+++ client/src/dashboard/sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
 import "./Sidebar.css";
+import Logo from "../../assets/dashboardImage/logo (1).png";
+
 const Sidebar = () => {
     return (
         <div className="Sidebar">
             {/* logo */}
             <div className="logo">
-                <img src="" alt="" />
-                <span></span>
+                <img src={Logo} alt="" />
+                <span>
+                    Sh<span>o</span>ps
+                </span>
             </div>
         </div>
     );
