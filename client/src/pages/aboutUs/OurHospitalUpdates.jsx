e2d51dbe0eed3b9fe45aa6822a8c3083da807ce7
jaheda8256 <jahedasultana47@gmail.com>
2024-11-20 16:34:05 +0600

    added image for about section

--- client/src/pages/aboutUs/OurHospitalUpdates.jsx
+++ client/src/pages/aboutUs/OurHospitalUpdates.jsx
@@ -16,34 +16,35 @@ const OurHospitalUpdates = () => {
       image: "https://i.postimg.cc/T2c8PvfM/cheerful-woman.jpg",
       question: "Why opt for our medical services for your family?",
       answer:
-        "Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+        "Our medical services prioritize patient care with a combination of expert professionals and state-of-the-art technology. From routine check-ups to advanced treatments, we ensure personalized attention for every family member. Our commitment to safety, efficiency, and compassion makes us the trusted choice for healthcare. With a focus on accessibility and affordability.",
     },
     {
       image: "https://i.postimg.cc/T2c8PvfM/cheerful-woman.jpg",
       question: "Why choose our medical for your family?",
       answer:
-        "Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+        "We offer comprehensive healthcare solutions tailored to meet your family’s needs. Our dedicated team of doctors and medical staff ensures reliable and effective treatments. With a patient-first approach, we provide a nurturing and welcoming environment for recovery and wellness. Trust us to deliver quality care and support for every stage of life, ensuring peace of mind for your loved ones.",
     },
     {
       image: "https://i.postimg.cc/T2c8PvfM/cheerful-woman.jpg",
       question: "Why opt for our healthcare services for your family?",
       answer:
-        "Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.Get latest news in your inbox. Consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+        "Your family deserves the best in healthcare, and that’s exactly what we deliver. Our advanced facilities and skilled professionals work tirelessly to provide high-quality care for all. From preventive care to specialized treatments, we cater to every need with precision and empathy. Choose us for a dependable healthcare partner focused on keeping your loved ones healthy and happy.",
     },
   ];
+  
 
   return (
     <div className="w-[90%] mx-auto my-28">
-        <div className="">
+      <div className="">
         <h2 className="text-[#1ABC9C] text-2xl mb-3">Our Hospital Updates</h2>
-        <div className="w-[80px] h-[2px] mb-6 bg-[#1ABC9C]"></div>
+        <div className="w-[80px] h-[2px] mb-8 bg-[#1ABC9C]"></div>
       </div>
 
       <div className="flex flex-col md:flex-row gap-6">
         {/* FAQ Section */}
         <div className="w-[72%]">
           {faqs.map((faq, index) => (
-            <div key={index} className="mb-4 bg-gray-200 border pb-2">
+            <div key={index} className="mb-4 bg-[#FFFAF9] border pb-2">
               <div
                 className="flex justify-between items-center cursor-pointer p-[14px]"
                 onClick={() => toggleFAQ(index)}
@@ -51,7 +52,7 @@ const OurHospitalUpdates = () => {
                 <h3 className="text-base font-medium flex items-center gap-4 transition-all duration-1000 delay-700">
                   <FaQuestionCircle
                     className={`text-lg ${
-                      open === index ? "text-[#1DBFCC]" : "text-red-700"
+                      open === index ? "text-[#1ABC9C]" : "text-[#FD9678]"
                     }`}
                   />
                   {faq.question}
@@ -82,8 +83,17 @@ const OurHospitalUpdates = () => {
           ))}
         </div>
 
-        <div className="w-[28%] ">
-          <img className="h-[406px] border-b-4 border-[#FD9678] rounded-lg" src="https://i.postimg.cc/ZY7K1tWH/cool-doctor.jpg" alt="" />
+
+        {/* image */}
+        <div className="w-[28%] group">
+          <div className="relative">
+            <img
+              className="h-[406px] w-full object-cover border-b-2 border-[#FD9678] rounded-lg"
+              src="https://i.postimg.cc/ZY7K1tWH/cool-doctor.jpg"
+              alt="Doctor"
+            />
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#FD9678] bg-opacity-10 rounded-lg transition-opacity duration-500 ease-in-out group-hover:bg-opacity-0"></div>
+          </div>
         </div>
       </div>
     </div>
