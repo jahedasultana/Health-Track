107fdd47236360680ea20a453b3465b5466b7ec3
jaheda8256 <jahedasultana47@gmail.com>
2024-11-29 11:19:36 +0600

    infoCard section with added bg-image

--- src/pages/home/specialties/Specialties.jsx
+++ src/pages/home/specialties/Specialties.jsx
@@ -4,31 +4,41 @@ import { FaHeartbeat, FaTooth, FaBrain, FaEye } from "react-icons/fa";
 
 const Specialties = () => {
   return (
-    <div className="w-[90%] space-y-2 pt-[30px] md:mt-[90px] mb-10 md:mb-28 mx-auto ">
-  
+    <div className="relative w-full ">
+      {/* Rotated Background Image */}
+      <div
+        className="absolute md:block hidden  -left-[100px] top-0 inset-0 h-[450px] w-[390px] bg-no-repeat bg-cover"
+        style={{
+          backgroundImage: `url('https://kamleshyadav.com/html/healthcare/bootstrap5/images/service/wedobg.png')`,
+        }}
+      ></div>
 
-      <div className=" mb-20">
-        <p className="text-black/70 text-[14px] font-semibold mb-2">Professionals</p>
-        <h2 className=" relative text-[#1ABC9C] md:text-4xl text-2xl font-bold mb-3 w-[max-content]">
-        Our Specialties
-          <TiPlus className="absolute text-[#FD9678]  -right-6 -top-4" />
-          <TiPlus className="absolute text-[#FD9678] opacity-70 text-2xl -right-[27px] rotate-45 -top-5" />
-        </h2>
-        <div className="w-[80px] h-[2px] mb-6 bg-[#1ABC9C]"></div>
-      </div>
+      {/* Content Section */}
+      <div className="relative w-[90%] space-y-2 pt-[30px] md:mt-[90px] mb-10 md:mb-28 mx-auto">
+        <div className="mb-20">
+          <p className="text-black/70 text-[14px] font-semibold mb-2">
+            Professionals
+          </p>
+          <h2 className="relative text-[#1ABC9C] md:text-4xl text-2xl font-bold mb-3 w-[max-content]">
+            Our Specialties
+            <TiPlus className="absolute text-[#FD9678] -right-6 -top-4" />
+            <TiPlus className="absolute text-[#FD9678] opacity-70 text-2xl -right-[27px] rotate-45 -top-5" />
+          </h2>
+          <div className="w-[80px] h-[2px] mb-6 bg-[#1ABC9C]"></div>
+        </div>
 
-
-      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-4 space-y-14 md:space-y-0">
-        {doctorCards.map((card) => (
-          <InfoCard
-            key={card.title}
-            title={card.title}
-            color={card.color}
-            Icon={card.Icon}
-            description={card.description}
-            isEven={card.isEven}
-          />
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-4 space-y-14 md:space-y-0">
+          {doctorCards.map((card) => (
+            <InfoCard
+              key={card.title}
+              title={card.title}
+              color={card.color}
+              Icon={card.Icon}
+              description={card.description}
+              isEven={card.isEven}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
