d34114454664046fe7afb466a9c06b47fb4861c4
jaheda8256 <jahedasultana47@gmail.com>
2024-11-30 22:15:30 +0600

    about us fix

--- src/pages/home/aboutUs/AboutUs.jsx
+++ src/pages/home/aboutUs/AboutUs.jsx
@@ -4,7 +4,7 @@ import sideImg from "../../../assets/homeImage/aboutme.png";
 
 const AboutUs = () => {
   return (
-    <div className=" md:w-[90%] md:my-32 my-20 w-full mx-auto">
+    <div className=" md:w-[90%]  w-full mx-auto">
       <div>
         <p className="text-black/70 text-[14px] font-semibold mb-2">About Us</p>
         <h2 className=" relative text-[#1ABC9C] md:text-4xl text-2xl font-bold mb-3 w-[max-content]">
@@ -13,12 +13,12 @@ const AboutUs = () => {
           <TiPlus className="absolute text-[#FD9678]  -right-6 -top-4" />
           <TiPlus className="absolute text-[#FD9678] opacity-70 text-2xl -right-[27px] rotate-45 -top-5" />
         </h2>
-        <div className="w-[80px] h-[2px] mb-6 bg-[#1ABC9C]"></div>
+        <div className="w-[80px] h-[2px] bg-[#1ABC9C]"></div>
       </div>
 
       <div className="flex flex-col md:flex-row items-center">
         {/* Text Section */}
-        <div className="w-full md:w-1/2 ">
+        <div className="w-full md:w-[40%] ">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
             perferendis totam, nobis vitae molestias, eaque corrupti ut commodi
@@ -27,7 +27,7 @@ const AboutUs = () => {
           </p>
         </div>
 
-        <div className="w-full md:w-1/2 h-full">
+        <div className="w-full md:w-[60%] flex justify-center items-center  h-full">
           <img src={mainImg} alt="images" />
         </div>
       </div>
