1bb4349adedf11cea0f5fe4c403b624915c05835
jaheda8256 <jahedasultana47@gmail.com>
2024-12-12 23:05:09 +0600

    added text

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -126,7 +126,7 @@ const Details = () => {
                     />
                     <button
                         type="submit"
-                        className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors"
+                        className="bg-blue-500  text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors"
                     >
                         Submit
                     </button>
