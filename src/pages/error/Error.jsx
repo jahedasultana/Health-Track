6073fe28073c5ca8dc0d7e81900f040dcfe82088
Jaheda Sultana <147721080+jahedasultana@users.noreply.github.com>
2025-01-04 23:14:49 +0600

    Update Error.jsx

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -3,7 +3,7 @@
 const Error = () => {
     return (
         <div>
-            <h1>this is error page</h1>
+            <h1>this is error page, are you back tohome page</h1>
             <Link to={'/'}>Back To Home Page</Link>
         </div>
     );
