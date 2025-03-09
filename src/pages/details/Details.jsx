04a25629c25346b371e5fc5b97af61aff740984c
jaheda8256 <jahedasultana47@gmail.com>
2025-03-09 08:00:00 +0600

    details page error and succec alart added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,189 +1,219 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Swal from "sweetalert2";
 import useAuth from "../../provider/useAuth";
 import useRole from "../../hooks/useRole";
 import PageBanner from "../../components/PageBanner";
 
 const Details = () => {
-    const { user } = useAuth()
-    const { id } = useParams();
-    const [doctors, setDoctor] = useState([]);
-    const [formData, setFormData] = useState({
+  const { user } = useAuth();
+  const { id } = useParams();
+  const [doctors, setDoctor] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+    condition: "",
+    details: "",
+  });
+
+  const email = user?.email;
+  const name = user?.displayName || "Guest";
+  const { role } = useRole();
+  const doctorEmail = doctors?.email;
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://health-track-server-ruddy.vercel.app/doctors/${id}`
+      );
+      setDoctor(response.data);
+    } catch (error) {
+      console.error("Error fetching doctors:", error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const userDetaild = {
+      ...formData,
+      name,
+      email,
+    };
+    const data = {
+      userDetaild,
+      doctorEmail,
+      userEmail: email,
+      status: "pending",
+    };
+
+    try {
+      const res = await axios.post(
+        "https://health-track-server-ruddy.vercel.app/service_request",
+        data
+      );
+      console.log(res.data);
+
+      // success
+      Swal.fire({
+        title: "Success!",
+        text: "Your request has been sent successfully.",
+        icon: "success",
+        confirmButtonText: "OK",
+      });
+
+      // reset
+      setFormData({
         name: "",
         age: "",
         condition: "",
         details: "",
-    });
-    const email = user?.email;
-    const name = user?.displayName || 'Guest';
-    const { role } = useRole()
-    const doctorEmail = doctors?.email;
-    console.log(doctorEmail);
+      });
+    } catch (error) {
+      console.log(error);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+      // error
+      Swal.fire({
+        title: "Error!",
+        text: "Something went wrong. Please try again.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    }
+  };
 
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(`https://health-track-server-ruddy.vercel.app/doctors/${id}`);
-            setDoctor(response.data);
-        } catch (error) {
-            console.error("Error fetching doctors:", error);
-        }
-    };
+  return (
+    <section className="min-h-screen ">
+      <PageBanner
+        title={"Details"}
+        pageAddress={`/getAvailableDoctor/${id}`}
+        pageDirection={"Details"}
+      />
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+      <div className="flex flex-col lg:flex-row gap-8 py-10 ">
+        {/* Doctor Details Section */}
+        <div className="flex-1 bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-4">Doctor Details</h2>
+          <div className="bg-gray-200 h-64 w-full mb-4 rounded-lg overflow-hidden flex items-center justify-center">
+            <img
+              src="https://i.postimg.cc/bJ7Jbx4T/doctor-girl.jpg"
+              alt="Doctor"
+              className="w-full h-full object-cover object-top"
+            />
+          </div>
+          <div className="text-gray-700 mb-4">
+            <p>
+              <span className="font-semibold">Name:</span> {doctors?.name}
+            </p>
+            <p>
+              <span className="font-semibold">Total Service:</span>{" "}
+              {doctors.service_experience}
+            </p>
+            <p>
+              <span className="font-semibold">Specialization:</span>{" "}
+              {doctors?.service_category}
+            </p>
+            <p>
+              <span className="font-semibold">Email :</span> {doctors?.email}
+            </p>
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        const userDetaild = {
-          ...formData,
-          name,
-          email,
-        };
-        const data = {
-          userDetaild, 
-          doctorEmail,
-          userEmail: email,
-          status: 'pending',
-        };
-      
-        try {
-          const res = await axios.post('https://health-track-server-ruddy.vercel.app/service_request', data);
-          console.log(res.data);
-          
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      
-
-    return (
-        <section className="min-h-screen ">
-                 <PageBanner title={'Details'} pageAddress={`/getAvailableDoctor/${id}`} pageDirection={'Details'} />
-
- <div className="flex flex-col lg:flex-row gap-8 py-10 ">
-            {/* Doctor Details Section */}
-            <div className="flex-1 bg-gray-100 p-6 rounded-lg shadow-md">
-                <h2 className="text-xl font-semibold mb-4">Doctor Details</h2>
-                <div className="bg-gray-200 h-64 w-full mb-4 rounded-lg overflow-hidden flex items-center justify-center">
-                    <img
-                        src="https://i.postimg.cc/bJ7Jbx4T/doctor-girl.jpg"
-                        alt="Doctor"
-                        className="w-full h-full object-cover object-top"
-                    />
-                </div>
-                <div className="text-gray-700 mb-4">
-                    <p><span className="font-semibold">Name:</span> {doctors?.name}</p>
-                    <p><span className="font-semibold">Total Service:</span> {doctors.service_experience}</p>
-                    <p><span className="font-semibold">Specialization:</span> {doctors?.service_category}</p>
-                    <p><span className="font-semibold">Email :</span> {doctors?.email}</p>
-
-                    <p>
-                        <span className="font-semibold">Availability :</span>{" "}
-                        <span
-                            className={`px-[3px] pb-[1px] rounded-md ${doctors?.availability === "busy"
-                                ? "bg-red-400/50 border border-red-600/80"
-                                : "bg-green-400/50 border border-green-600/50"
-                                }`}
-                        >
-                            {doctors?.availability}
-                        </span>
-                    </p>
-                </div>
-                <div className="bg-gray-300/90 p-3 rounded-lg">
-                    <h4 className="font-semibold mb-2">Which Services:</h4>
-                    <p>{doctors.service_give}</p>
-                </div>
-            </div>
-
-            {/* Patient Details Form Section */}
-            <div className="flex-1 bg-gray-100 p-6 rounded-lg shadow-md">
-                <h2 className="text-xl font-semibold mb-4">Patient Details - Input Form</h2>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                    {/* Row for Patient Name and Age */}
-                    <div className="flex gap-4">
-                        <input
-                            type="text"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleInputChange}
-                            placeholder="Patient Name"
-                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                        <input
-                            type="text"
-                            name="age"
-                            value={formData.age}
-                            onChange={handleInputChange}
-                            placeholder="Patient Age"
-                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-
-                    <input
-                        type="text"
-                        name="condition"
-                        value={formData.condition}
-                        onChange={handleInputChange}
-                        placeholder="Patient Condition"
-                        className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-
-                    <textarea
-                        name="details"
-                        value={formData.details}
-                        onChange={handleInputChange}
-                        placeholder="Additional Details"
-                        className="w-full h-[170px] resize-none p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                    {
-                        doctors?.availability == 'busy' ?
-                            <button
-                                type="submit"
-                                disabled={doctors?.availability == 'busy'}
-                                className={`text-white py-2 px-4 rounded-lg
-                        ${doctors?.availability == 'busy' && 'cursor-not-allowed bg-slate-500'}
-                        transition-colors`}
-                            >
-                                Not Allow
-                            </button>
-
-                            :
-                            <button
-                                type="submit"
-                                disabled={role === 'doctor'}
-                                className={`text-white py-2 px-4 rounded-lg
-                                ${role === 'doctor' ? 'cursor-not-allowed bg-slate-500' : 'cursor-auto bg-[#FD9678] hover:bg-[#1ABC9C]'}
-                                transition-colors cursor-pointer`}
-                            >
-                                Submit
-                            </button>
-
-                    }
-                    {/* <button
-                        type="submit"
-                        disabled={role === 'doctor'}
-                        className={`text-white py-2 px-4 rounded-lg
-                        ${role === 'doctor' ? 'cursor-not-allowed bg-slate-500' : 'cursor-auto bg-blue-500'}
-                        transition-colors`}
-                        title={role === 'doctor' ? 'You are a doctor' : ''}
-                    >
-                        Submit
-                    </button> */}
-
-                </form>
-            </div>
+            <p>
+              <span className="font-semibold">Availability :</span>{" "}
+              <span
+                className={`px-[3px] pb-[1px] rounded-md ${
+                  doctors?.availability === "busy"
+                    ? "bg-red-400/50 border border-red-600/80"
+                    : "bg-green-400/50 border border-green-600/50"
+                }`}
+              >
+                {doctors?.availability}
+              </span>
+            </p>
+          </div>
+          <div className="bg-gray-300/90 p-3 rounded-lg">
+            <h4 className="font-semibold mb-2">Which Services:</h4>
+            <p>{doctors.service_give}</p>
+          </div>
         </div>
-        </section>
-    
-    );
+
+        {/* Patient Details Form Section */}
+        <div className="flex-1 bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-4">
+            Patient Details - Input Form
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            {/* Row for Patient Name and Age */}
+            <div className="flex gap-4">
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                placeholder="Patient Name"
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <input
+                type="text"
+                name="age"
+                value={formData.age}
+                onChange={handleInputChange}
+                placeholder="Patient Age"
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <input
+              type="text"
+              name="condition"
+              value={formData.condition}
+              onChange={handleInputChange}
+              placeholder="Patient Condition"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+
+            <textarea
+              name="details"
+              value={formData.details}
+              onChange={handleInputChange}
+              placeholder="Additional Details"
+              className="w-full h-[170px] resize-none p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {doctors?.availability === "busy" ? (
+              <button
+                type="submit"
+                disabled
+                className="text-white py-2 px-4 rounded-lg cursor-not-allowed bg-slate-500 transition-colors"
+              >
+                Not Allowed
+              </button>
+            ) : (
+              <button
+                type="submit"
+                disabled={role === "doctor"}
+                className={`text-white py-2 px-4 rounded-lg ${
+                  role === "doctor"
+                    ? "cursor-not-allowed bg-slate-500"
+                    : "cursor-auto bg-[#FD9678] hover:bg-[#1ABC9C]"
+                } transition-colors cursor-pointer`}
+              >
+                Submit
+              </button>
+            )}
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Details;
--- src/pages/doctorService/DoctorService.jsx
+++ src/pages/doctorService/DoctorService.jsx
@@ -2,117 +2,124 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const DoctorService = () => {
-    const email = localStorage.getItem("userRoleEmail");
-    const [doctorDetails, setDoctorDetails] = useState();
+  const email = localStorage.getItem("userRoleEmail");
+  const [doctorDetails, setDoctorDetails] = useState([]);
 
-    console.log("Doctor Details:", doctorDetails);
+  console.log("Doctor Details:", doctorDetails);
 
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(
-                `https://health-track-server-ruddy.vercel.app/doctor_service/${email}`
-            );
-            setDoctorDetails(response.data); // Assuming the backend returns the relevant document
-        } catch (error) {
-            console.error("Error fetching user data:", error);
-        }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/doctor_service/${email}`
+      );
+      setDoctorDetails(response.data); // Assuming the backend returns the relevant document
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
 
-    const updateStatus = async (id, newStatus) => {
+  const updateStatus = async (id, newStatus) => {
+    console.log(newStatus);
+    try {
+      const response = await axios.put(
+        `https://health-track-server-ruddy.vercel.app/doctor_service/update_status/${id}`,
+        { status: newStatus }
+      );
 
-        console.log(newStatus);
-        try {
-            const response = await axios.put(
-                `https://health-track-server-ruddy.vercel.app/doctor_service/update_status/${id}`,
-                { status: newStatus }
-            );
+      if (response.status === 200) {
+        alert("Status updated successfully!");
+        fetchData(); // Refresh the data after the update
+      }
+    } catch (error) {
+      console.error("Error updating status:", error);
+    }
+  };
 
-            if (response.status === 200) {
-                alert("Status updated successfully!");
-                fetchData(); // Refresh the data after the update
-            }
-        } catch (error) {
-            console.error("Error updating status:", error);
-        }
-    };
+  useEffect(() => {
+    if (email) {
+      fetchData();
+    }
+  }, [email]);
 
-    useEffect(() => {
-        if (email) {
-            fetchData();
-        }
-    }, [email]);
-
-    return (
-        <div className="bg-gray-100 min-h-screen md:p-8">
-            <div className="bg-white shadow-md rounded-md p-6 max-w-4xl mx-auto">
-                <div className="text-center">
-                    <div className="bg-green-500 w-24 h-24 rounded-full mx-auto"></div>
-                    <h1 className="text-2xl font-bold mt-4">User Details</h1>
-                    <p className="text-gray-600">{email}</p>
-                </div>
-                <div className="mt-8">
-                    <h2 className="text-xl font-semibold mb-4">User Details</h2>
-                    <div className="overflow-x-auto">
-                        <table className="w-full border-collapse border border-gray-300 text-left">
-                            <thead>
-                                <tr className="bg-gray-200">
-                                    <th className="border border-gray-300 px-4 py-2">Name</th>
-                                    <th className="border border-gray-300 px-4 py-2">Age</th>
-                                    <th className="border border-gray-300 px-4 py-2">Condition</th>
-                                    <th className="border border-gray-300 px-4 py-2">Details</th>
-                                    <th className="border border-gray-300 px-4 py-2">Status</th>
-                                    <th className="border border-gray-300 px-4 py-2">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td className="border border-gray-300 px-4 py-2">
-                                        {doctorDetails?.name || "N/A"}
-                                    </td>
-                                    <td className="border border-gray-300 px-4 py-2">
-                                        {doctorDetails?.age || "N/A"}
-                                    </td>
-                                    <td className="border border-gray-300 px-4 py-2">
-                                        {doctorDetails?.condition || "N/A"}
-                                    </td>
-                                    <td className="border border-gray-300 px-4 py-2">
-                                        {doctorDetails?.details || "N/A"}
-                                    </td>
-                                    <td className="border border-gray-300 px-4 py-2">
-                                        <span className={doctorDetails?.status === "pending" ? 'bg-orange-300 p-2 rounded-lg' : "bg-green-300 p-2 rounded-lg"}>
-                                            {doctorDetails?.status || "N/A"}
-                                        </span>
-                                    </td>
-                                    <td className="border border-gray-300 px-4 py-2">
-                                        <button
-                                            className="p-1 bg-green-400 rounded-lg"
-                                            onClick={() =>
-                                                updateStatus(
-                                                    doctorDetails?._id,
-                                                    doctorDetails?.status === "Pending"
-                                                        ? "Done"
-                                                        : "Pending"
-                                                )
-                                            }
-                                        >
-                                            {doctorDetails?.status === "Pending"
-                                                ? "Mark as Done"
-                                                : "Mark as Pending"}
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                </div>
-                <p className="text-gray-500 mt-6">
-                    For further updates, check your patient portal regularly.
-                </p>
-            </div>
-
+  return (
+    <div className="bg-gray-100 min-h-screen md:p-8">
+      <div className="bg-white shadow-md rounded-md p-6 max-w-4xl mx-auto">
+        <div className="text-center">
+          <div className="bg-green-500 w-24 h-24 rounded-full mx-auto"></div>
+          <h1 className="text-2xl font-bold mt-4">User Details</h1>
+          <p className="text-gray-600">{email}</p>
         </div>
-    );
+        <div className="mt-8">
+          <h2 className="text-xl font-semibold mb-4">User Details</h2>
+          <div className="overflow-x-auto">
+            <table className="w-full border-collapse border border-gray-300 text-left">
+              <thead>
+                <tr className="bg-gray-200">
+                  <th className="border border-gray-300 px-4 py-2">Name</th>
+                  <th className="border border-gray-300 px-4 py-2">Age</th>
+                  <th className="border border-gray-300 px-4 py-2">
+                    Condition
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2">Details</th>
+                  <th className="border border-gray-300 px-4 py-2">Status</th>
+                  <th className="border border-gray-300 px-4 py-2">Action</th>
+                </tr>
+              </thead>
+             {
+                doctorDetails.map((doctor) =>(  <tbody key={doctor._id}>
+                    <tr>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {doctor?.name || "N/A"}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {doctor?.age || "N/A"}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {doctor?.condition || "N/A"}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {doctor?.details || "N/A"}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        <span
+                          className={
+                            doctor?.status === "pending"
+                              ? "bg-orange-300 p-2 rounded-lg"
+                              : "bg-green-300 p-2 rounded-lg"
+                          }
+                        >
+                          {doctor?.status || "N/A"}
+                        </span>
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        <button
+                          className="p-1 bg-green-400 rounded-lg"
+                          onClick={() =>
+                            updateStatus(
+                              doctor?._id,
+                              doctor?.status === "Pending"
+                                ? "Done"
+                                : "Pending"
+                            )
+                          }
+                        >
+                          {doctor?.status === "Pending"
+                            ? "Mark as Done"
+                            : "Mark as Pending"}
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>))
+             }
+            </table>
+          </div>
+        </div>
+        <p className="text-gray-500 mt-6">
+          For further updates, check your patient portal regularly.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default DoctorService;
